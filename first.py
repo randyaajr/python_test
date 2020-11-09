276ca9d5597f3f8687a368b9ec702c54e50136be
randyaajr <randyaajrsp@gmail.com>
2020-11-09 19:50:22 +0000

    Final commit.

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
 from flask import Flask
+
+
+"""
+Topics i went over in this test
+
+1. Install packages from the Python Package index using: pip3 install
+2. See what dependencies are installed and, optionally, redirect the output to a requirements file: pip3 freeze --local
+3. Install dependencies from a requirements file: pip3 install -r requirements.txt
+"""
